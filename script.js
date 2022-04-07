37e2488909fba5cc56f7debddf4588fbd8821b72
Aisha Njau <aishanjau@10-23-0-239.dynapool.nyu.edu>
2022-04-06 22:52:46 -0400

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+var output;
+var input;
+var restyleButton;
+
+document.getElementById("half-button").addEventListener("click",halfNum);
+  function halfNum(){
+  var input = document.getElementById("theInput").value;
+  alert("Half of " + input + " is " + input/2);
+  }
+  document.addEventListener("DOMContentLoaded",function(){
+    var output = document.getElementById('outputText');
+    var restyleButton = document.getElementById("restyle-button");
+ }
